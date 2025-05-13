d5229d552766af3479eebeadc4b06260290b8a13
Robert Hasinbiller <rkhasinbiller@gmail.com>
2025-05-13 14:05:26 -0500

    Upload hw

--- fleet.js
+++ fleet.js
@@ -28,3 +28,11 @@ let car = new fleetVehicles("car", "chevy", "malibu", 2008);
 let truck = new fleetVehicles("truck", "Ford", "F150", 2023);
 
 let motorcycle = new fleetVehicles("motorcycle", "Harley", "Davidson", 2018);
+
+car.drive(250);
+truck.drive(375);
+motorcycle.drive(520);
+
+console.log(car.getDetails());
+console.log(truck.getDetails());
+console.log(motorcycle.getDetails());
