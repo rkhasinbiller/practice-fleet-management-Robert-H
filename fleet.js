d364856f06a8ae86bd8b2bf7490d67fb535fa373
Robert Hasinbiller <rkhasinbiller@gmail.com>
2025-05-13 12:48:24 -0500

    Upload hw

---
+++ fleet.js
@@ -0,0 +1,10 @@
+class fleetVehicles {
+  constructor(type, make, model, year, mileage = 0) {
+    this.type = type;
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.mileage = mileage;
+  }
+}
+
