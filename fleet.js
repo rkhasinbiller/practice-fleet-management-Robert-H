cd7d0e0fbd1f4bdcd40dc1f91eefa7b0460f0490
Robert Hasinbiller <rkhasinbiller@gmail.com>
2025-05-13 13:58:18 -0500

    Upload hw

--- fleet.js
+++ fleet.js
@@ -6,5 +6,25 @@ class fleetVehicles {
     this.year = year;
     this.mileage = mileage;
   }
+
+
+drive(distance) {
+  if (distance > 0) {
+    this.mileage += distance;
+ console.log(`The ${this.make} ${this.model} drove ${distance} miles`); 
+} else {
+  console.log("Distance must be a positive number.");
+}
 }
 
+getDetails () {
+  return `${this.type}, Make: ${this.make}, Model: ${this.model}, Year: ${this.year}, Mileage: ${this.mileage}`;
+}
+
+}
+
+let car = new fleetVehicles("car", "chevy", "malibu", 2008);
+
+let truck = new fleetVehicles("truck", "Ford", "F150", 2023);
+
+let motorcycle = new fleetVehicles("motorcycle", "Harley", "Davidson", 2018);
